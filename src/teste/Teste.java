e8f1fb87c186d4bec6d23d886688af082d79c371
viniaquino <contato.viniaquino@outlook.com>
2024-11-12 10:25:52 -0300

    teste2

---
+++ src/teste/Teste.java
@@ -0,0 +1,20 @@
+package teste;
+
+public class Teste {
+
+	private String teste;
+
+	public Teste(String teste) {
+		super();
+		this.teste = teste;
+	}
+
+	public String getTeste() {
+		return teste;
+	}
+
+	public void setTeste(String teste) {
+		this.teste = teste;
+	}
+
+}
